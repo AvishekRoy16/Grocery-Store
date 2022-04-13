da0b32ebd197bf59f8ba2084b658f590b6e2c9e5
adarshcode12 <adarshcode12@gmail.com>
2022-04-14 03:47:28 +0530

    decrement and remove cart item

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from os import TMP_MAX
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from store_product.models import Product
 from .models import Cart, CartItem
 # Create your views here.
@@ -34,6 +34,29 @@ def add_cart(request, product_id):
         )
         cart_item.save()
     return redirect('cart')
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id =_cart_id(request))
+    product = get_object_or_404(Product,id = product_id)
+
+    cart_item = CartItem.objects.get(product=product, cart = cart)
+    print(cart_item.quantity)
+    if(cart_item.quantity>1):
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id =_cart_id(request))
+    product = get_object_or_404(Product,id = product_id)
+    cart_item = CartItem.objects.get(product=product, cart = cart)    
+    cart_item.delete()
+    return redirect('cart')
+
+
+
 def cart(request):
     return render(request, "store_product/cart.html")
 
