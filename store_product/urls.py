d9df50143dcff2c4db48d9bf4abc2559591de90f
Abhimanyu Kumar <abhimanyuthebes1@gmail.com>
2022-04-13 22:34:43 +0530

    Added link routs and images using superuser

--- store_product/urls.py
+++ store_product/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.store_product,name='store_product'),
-    path('<slug:category_slug>/',views.store_product,name='product_by_category')
+    path('<slug:category_slug>/',views.store_product,name='product_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>',views.product_detail,name='product_detail')
 ]
--- store_product/views.py
+++ store_product/views.py
@@ -21,3 +21,13 @@ def store_product(request,category_slug = None):
 
     }
     return render(request,'store_product.html',context)
+
+def product_detail(request,category_slug,product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug,slug = product_slug)
+    except Exception as e:
+        raise e
+    context = {
+        'single_product' : single_product,
+    }
+    return render(request, 'store_product/product_detail.html',context)
