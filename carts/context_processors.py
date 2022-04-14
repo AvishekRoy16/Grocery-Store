e4b94162104ba913f3e97f6184a81eb270dad180
Abhimanyu Kumar <abhimanyuthebes1@gmail.com>
2022-04-14 15:29:38 +0530

    cart functionality of product_count

--- carts/context_processors.py
+++
@@ -1,8 +0,0 @@
-# from .models import Cart, CartItem
-
-# def counter (request):
-#     if "admin" in request.path:
-#         return {}
-#     else:
-#         try:
-#             cart = Cart.objects.filter()
