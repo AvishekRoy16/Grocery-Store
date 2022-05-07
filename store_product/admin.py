be16c8b11ad625776a61246bbf365a249547549b
abhinav-1707 <abhinavsingh194.as@gmail.com>
2022-05-08 01:39:33 +0530

    GS-21 More to be done on variation

--- store_product/admin.py
+++ store_product/admin.py
@@ -5,5 +5,12 @@ from .models import Product, Variation
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('product_name',)}
     list_display = ['product_name','slug','price','stock','category','modified_date','is_available']
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value')
+
 admin.site.register(Product,ProductAdmin)
-admin.site.register(Variation)
+admin.site.register(Variation,VariationAdmin)
+  
